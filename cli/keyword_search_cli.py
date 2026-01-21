e10244deb68a73589c363b57475b7c4d417cefa4
Yuxiang <yuxiang32@gmail.com>
2026-01-21 23:20:25 +0800

    Update Python version requirement
    Bump the minimum Python version requirement to 3.12 in pyproject.toml
    and uv.lock. This change ensures compatibility with newer Python
    features and libraries.

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+import json
 
 
 def main() -> None:
@@ -14,11 +15,24 @@ def main() -> None:
 
     match args.command:
         case "search":
-            # print the search query here
             print(f"Searching for: {args.query}")
-            pass
+            data = load_data("data/movies.json")
+            movies_results = []
+            for movie in data["movies"]:
+                if args.query.lower() in movie["title"].lower():
+                    movies_results.append(movie)
+            movie_sorted = sorted(movies_results, key=lambda x: x["id"])
+            five_movies = movie_sorted[:5]
+            for movie in five_movies:
+                print(f"{movie['id']}. {movie['title']}")
         case _:
             parser.print_help()
+
+
+def load_data(file_path: str) -> dict:
+    with open(file_path, "r") as file:
+        data = json.load(file)
+    return data
 
 
 if __name__ == "__main__":
